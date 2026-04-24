b31988aec1a398079c4b46d28adb0d0de1ab3a6b
vigneshwaranit56 <vigneshwaranit56@gmail.com>
2026-04-24 13:29:57 +0530

    added terraform for the same setup

---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,19 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      desired_size   = 2
+      min_size       = 1
+      max_size       = 3
+    }
+  }
+}
